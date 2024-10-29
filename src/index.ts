7cc27130cec5ed979394f9d2de712beae4747a87
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2024-10-29 19:27:11 +0100

    Create config by environment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-
+import config from './config';
 import app from './server';
 
 // Error handling outside of the controllers
@@ -13,6 +13,6 @@ process.on('uncaughtException', (error) => {
   console.error('Uncaught Exception:', error);
 })
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
 });
